f1a1366977dbf54d0665611b2b0571f54d3bd8cf
tumg <tumg@dayoodeMacBook-Air.local>
2015-12-05 10:29:34 +0800

    Initial Commit

---
+++ DYTRouterManagerDemo/DYTRouterManager/DYTRouterManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DYTRouterManager
+//
+//  Created by tumg on 15/12/5.
+//  Copyright (c) 2015年 tumg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DYTRouterManagerDemo/DYTRouterManager/DYTRouterManager/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DYTRouterManager
+//
+//  Created by tumg on 15/12/5.
+//  Copyright (c) 2015年 tumg. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
